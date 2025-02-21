f4f4de5f04272a694cd3d813610c837fa6bf0683
Atho1218 <125186173+Atho1218@users.noreply.github.com>
2025-02-22 01:11:59 +0200

    added the API immigration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "Roses are red violets are blue",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsElement = document.querySelector("#user-instructions");
+  let apiKey = "60at455d3d23aedc50eab463bf1f843o";
+  let context = `Generate a poem about ${instructionsElement.value}`;
+  let prompt =
+    "You are an amazing and love to write poetry. your mission is to generate a 4 line poem and separate each line with a <br/>, based on the topic the user provides. Make sure to stick to the user topic";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem");
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
