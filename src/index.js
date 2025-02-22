1eccaf2d78bccee8327f6e18cf214de116310b30
Atho1218 <125186173+Atho1218@users.noreply.github.com>
2025-02-22 02:21:59 +0200

    fixed it

--- src/index.js
+++ src/index.js
@@ -10,18 +10,18 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
 
-  let instructionsElement = document.querySelector("#user-instructions");
+  let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "60at455d3d23aedc50eab463bf1f843o";
-  let context = `Generate a poem about ${instructionsElement.value}`;
+  let context = `Generate a poem about ${instructionsInput.value}`;
   let prompt =
     "You are an amazing and love to write poetry. your mission is to generate a 4 line poem and separate each line with a <br/>, based on the topic the user provides. Make sure to stick to the user topic";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  axios.get(apiUrl).then(displayPoem);
+
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `⌛Generating Poem about ${instructionsElement.value}`;
-
-  axios.get(apiUrl).then(displayPoem);
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
